16ed74a49d355546b54b224b7cc3af35a71d98e8
Daniel Enrico Botta <danielenricobotta@outlook.it>
2022-08-02 23:02:22 +0200

    stuffff

---
+++ FishBrah/Extensions/FileExtensions.cs
@@ -0,0 +1,12 @@
+﻿using ImageMagick;
+
+namespace FishBrah.Extensions;
+
+public static class FileExtensions
+{
+    public static async Task WriteToFile(this MagickImage image, string name)
+    {
+        var fileInfo = new FileInfo($"{name}.jpg");
+        await image.WriteAsync(fileInfo.FullName);
+    }
+}
---
+++ FishBrah/Helpers/ImageHelper.cs
@@ -0,0 +1,15 @@
+﻿using ImageMagick;
+
+namespace FishBrah.Helpers;
+
+public static class ImageHelper
+{
+    public static MagickImage PrepareImage(string path)
+    {
+        var image = new MagickImage(path);
+        const int width = 200;
+        var height = width * image.Height / image.Width;
+        image.Scale(width, height);
+        return image;
+    }
+}
